a158a4f622c409adc31f1463d1ad4888479f9c28
Carlos A <carlosmiranda19122@gmail.com>
2021-09-27 11:31:44 -0300

    requisicao assincrona para recuperar os cursos a partir do tipo

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,38 @@
 const User = require('../models/User')
 const Certificados = require('../models/Certificados')
 
-exports.cadastro = function(req, res) {
-    let user = new User ()
-    user.recuperarCursos()
-    .then(function(cursos_recuperado){
-        res.render('pages/cadastro', { cursos_recuperados: cursos_recuperado , layout: 'pages/cadastro' })
-    })
-    .catch(function(err){
-        res.send(err)
-    })
+exports.cadastro = function (req, res) {
+    let user = new User()
+    user.recuperarTiposCursos()
+        .then(function (tipos_cursos_recuperados) {
+            res.render('pages/cadastro', { tipos_cursos_recuperado: tipos_cursos_recuperados, layout: 'pages/cadastro' })
+        })
+        .catch(function (err) {
+            res.send(err)
+        })
 }
 
-exports.login_form = function(req, res) {
+exports.cursos_json = function (req, res) {
+    let user = new User()
+    user.recuperarCursos(req.params.id_tipo_curso_fk)
+        .then(function (cursos_recuperados) {
+            // res.render('pages/cadastro', { tipos_cursos_recuperado: cursos_recuperados, layout: 'pages/cadastro' })
+            res.json({ cursos_recuperados: cursos_recuperados })
+        })
+        .catch(function (err) {
+            res.send(err)
+        })
+}
+
+exports.login_form = function (req, res) {
     res.render('pages/login', { layout: 'pages/login' })
 }
 
-
-
-exports.login = function(req, res) {
+exports.login = function (req, res) {
     let user = new User(req.body);
     user
         .login()
-        .then(function(result) {
+        .then(function (result) {
             console.log(result);
             req.session.user = {
                 nome: usuarioRecuperado.nome,
@@ -39,23 +49,22 @@ exports.login = function(req, res) {
                 horas_aes: usuarioRecuperado.horas_aes,
             }
 
-            req.session.save(function() {
+            req.session.save(function () {
                 res.redirect('/home')
             })
         })
-        .catch(function(err) {
+        .catch(function (err) {
             res.send(err)
         })
 }
 
-
-exports.logout = function(req, res) {
-    req.session.destroy(function() {
+exports.logout = function (req, res) {
+    req.session.destroy(function () {
         res.redirect("/")
     })
 }
 
-exports.mustBeLoggedIn = function(req, res, next) {
+exports.mustBeLoggedIn = function (req, res, next) {
     if (req.session.user) {
         next()
     } else {
@@ -63,12 +72,12 @@ exports.mustBeLoggedIn = function(req, res, next) {
     }
 }
 
-exports.esqueciASenha = function(req, res) {
+exports.esqueciASenha = function (req, res) {
     res.render('pages/esqueciAsenha', { layout: 'pages/esqueciAsenha' })
 
 }
 
-exports.home = function(req, res) {
+exports.home = function (req, res) {
     if (req.session.user) {
         res.render('pages/home')
     } else {
@@ -76,62 +85,60 @@ exports.home = function(req, res) {
     }
 }
 
-exports.atividadesComplementares = function(req, res) {
+exports.atividadesComplementares = function (req, res) {
     res.render('pages/atividadesComplementares')
 }
 
-
-exports.extensao = function(req, res) {
+exports.extensao = function (req, res) {
     res.render('pages/extensao')
 }
 
-exports.estatisticas = function(req, res) {
+exports.estatisticas = function (req, res) {
     res.render('pages/estatisticas')
 }
 
-exports.perfilDoAluno = function(req, res) {
+exports.perfilDoAluno = function (req, res) {
     if (req.session.user) {
         res.render('pages/perfilDoAluno')
     } else {
         res.render('pages/perfilDoAluno')
     }
 }
-exports.alterarDados = function(req, res) {
+
+exports.alterarDados = function (req, res) {
     let user = new User(req.body);
     user
-        .alterarDados(),user.readByEmail()
-        .then(function(result) {
-            req.session.user = {
-                nome: usuarioRecuperado.nome,
-                sobrenome: usuarioRecuperado.sobrenome,
-                curso: usuarioRecuperado.curso,
-                email: user.data.email,
-                cpf: usuarioRecuperado.cpf,
-                telefone: usuarioRecuperado.telefone,
-                instituicao: usuarioRecuperado.instituicao,
-                cidade: usuarioRecuperado.cidade,
-                senha: user.data.senha,
-                nascimento: usuarioRecuperado.nascimento,
-                horas_acs: usuarioRecuperado.horas_acs,
-                horas_aes: usuarioRecuperado.horas_aes,
-            }
-            res.redirect('/perfilDoAluno')
-        })
-        .catch(function(err) {
-            res.send(err)
-        })
+        .alterarDados(), user.readByEmail()
+            .then(function (result) {
+                req.session.user = {
+                    nome: usuarioRecuperado.nome,
+                    sobrenome: usuarioRecuperado.sobrenome,
+                    curso: usuarioRecuperado.curso,
+                    email: user.data.email,
+                    cpf: usuarioRecuperado.cpf,
+                    telefone: usuarioRecuperado.telefone,
+                    instituicao: usuarioRecuperado.instituicao,
+                    cidade: usuarioRecuperado.cidade,
+                    senha: user.data.senha,
+                    nascimento: usuarioRecuperado.nascimento,
+                    horas_acs: usuarioRecuperado.horas_acs,
+                    horas_aes: usuarioRecuperado.horas_aes,
+                }
+                res.redirect('/perfilDoAluno')
+            })
+            .catch(function (err) {
+                res.send(err)
+            })
 }
 
-
-
-exports.cadastrar = function(req, res) {
+exports.cadastrar = function (req, res) {
     // console.log(req.body);
     let user = new User(req.body);
     user.create()
-        .then(function(result) {
+        .then(function (result) {
             res.render('pages/login', { layout: 'pages/login' });
         })
-        .catch(function(err) {
+        .catch(function (err) {
             res.send(err);
         });
 };
